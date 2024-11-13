0e2a7a558e6d413b053fdabb8d58e349e138edf4
ucelery <atondavid.ad@gmail.com>
2024-11-13 16:34:35 +0800

    Finalize Dashboard

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -1,63 +1,87 @@
-import { useState } from 'react'
-import background from './assets/images/background.png'
-import logo from './assets/images/kain-ta-Logo.png'
-import { borderBottom, borderRadius, display, height, minHeight, width } from '@mui/system'
-import Button from '@mui/material/Button';
+import { useState, CSSProperties } from "react";
+import background from "./assets/images/background.png";
+import logo from "./assets/images/kain-ta-Logo.png";
+import {
+  borderBottom,
+  borderRadius,
+  display,
+  height,
+  maxHeight,
+  minHeight,
+  width,
+} from "@mui/system";
+import Button from "@mui/material/Button";
+
+import './dashboard.css'
 
 function Dashboard() {
-  enum Tab { Users, Establishments, Food }
+  enum Tab {
+    Users,
+    Establishments,
+    Food,
+  }
 
   const [selectedTab, setSelectedTab] = useState<Tab>(Tab.Users);
 
   const sidebar_buttons = [
-    <Button style={{...styles.sidebar_button}}>Moderation</Button>,
+    <Button style={{ ...styles.sidebar_button }}>Moderation</Button>,
     <Button style={styles.sidebar_button}>Verification</Button>,
     <Button style={styles.sidebar_button}>Analytics</Button>,
   ];
 
   const mstyles = {
-    button: { 
-      outline: 'none',
-            '&:focus': {
-              outline: 'none',
-              boxShadow: 'none', // Removes focus ring shadow
-            },
-      borderBottom: selectedTab === Tab.Establishments ? '2px solid black' : 'none' }
-  }
+    button: {
+      outline: "none",
+      "&:focus": {
+        outline: "none",
+        boxShadow: "none", // Removes focus ring shadow
+      },
+      borderBottom:
+        selectedTab === Tab.Establishments ? "2px solid black" : "none",
+    },
+  };
 
   const tab_buttons = [
-    <Button 
-      style={{...styles.tab_button}} 
-      sx={{ 
-        outline: 'none',
-        '&:focus': {
-        outline: 'none',
-        boxShadow: 'none', // Removes focus ring shadow
+    <Button
+      style={{ ...styles.tab_button }}
+      sx={{
+        outline: "none",
+        "&:focus": {
+          outline: "none",
+          boxShadow: "none", // Removes focus ring shadow
         },
-        borderBottom: selectedTab === Tab.Users ? '2px solid black' : 'none' 
+        borderBottom: selectedTab === Tab.Users ? "2px solid black" : "none",
       }}
-      onClick={() => setSelectedTab(Tab.Users)}>
-        Users
+      onClick={() => setSelectedTab(Tab.Users)}
+    >
+      Users
     </Button>,
-    <Button 
-    style={{...styles.tab_button}} 
-    sx={mstyles.button}
-    onClick={() => setSelectedTab(Tab.Establishments)}>
+    <Button
+      style={{ ...styles.tab_button }}
+      sx={mstyles.button}
+      onClick={() => setSelectedTab(Tab.Establishments)}
+    >
       Establishments
-  </Button>,
-    <Button 
-    style={{...styles.tab_button}} 
-    sx={{ 
-      outline: 'none',
-            '&:focus': {
-              outline: 'none',
-              boxShadow: 'none', // Removes focus ring shadow
-            },
-      borderBottom: selectedTab === Tab.Food ? '2px solid black' : 'none' }}
-    onClick={() => setSelectedTab(Tab.Food)}>
+    </Button>,
+    <Button
+      style={{ ...styles.tab_button }}
+      sx={{
+        outline: "none",
+        "&:focus": {
+          outline: "none",
+          boxShadow: "none", // Removes focus ring shadow
+        },
+        borderBottom: selectedTab === Tab.Food ? "2px solid black" : "none",
+      }}
+      onClick={() => setSelectedTab(Tab.Food)}
+    >
       Food
-  </Button>,
-    <Button style={{...styles.tab_button, background: '#2673DD', color: 'white'}}>add</Button>,
+    </Button>,
+    <Button
+      style={{ ...styles.tab_button, background: "#2673DD", color: "white" }}
+    >
+      add
+    </Button>,
   ];
 
   return (
@@ -65,213 +89,154 @@ function Dashboard() {
       <div style={styles.modal}>
         <div style={styles.container}>
           <div style={styles.sidebar}>
-            <img style={{ width: '70%', height: 'auto', borderRadius: '50%', marginTop: 77 }} src={logo} alt="" />
+            <img
+              style={{
+                width: "70%",
+                height: "auto",
+                borderRadius: "50%",
+                marginTop: 77,
+              }}
+              src={logo}
+              alt=""
+            />
 
-            <div style={{marginTop: 35}}>
-                {sidebar_buttons}
-            </div>
+            <div style={{ marginTop: 35 }}>{sidebar_buttons}</div>
           </div>
-          <div style={{width: '100%'}}>
-            <div style={{...styles.header, ...{textAlign: 'center'}}}>KAIN-TA SUPER ADMIN DASHBOARD</div>
-            <div style={{marginLeft: 25, marginRight: 25}}>
-              {/* HEADER */}
-              <div style={{height: 50}}>
-                <div style={{...{float: 'left'}}}>
-                    {tab_buttons[0]}
+          
+          <div className="table">
+            <div style={{ ...styles.header, ...{ textAlign: "center" } }}>
+              KAIN-TA SUPER ADMIN DASHBOARD
+            </div>
+            <div style={{ height: 50 }}>
+                <div style={{ ...{ float: "left" } }}>{tab_buttons[0]}</div>
+                <div
+                  style={{
+                    ...{ marginLeft: 10, marginRight: 10, float: "left" },
+                  }}
+                >
+                  {tab_buttons[1]}
                 </div>
-                <div style={{...{marginLeft: 10, marginRight: 10, float: 'left'}}}>
-                    {tab_buttons[1]}
-                </div>
-                <div style={{...{marginLeft: 10, marginRight: 10, float: 'left'}}}>
-                    {tab_buttons[2]}
+                <div
+                  style={{
+                    ...{ marginLeft: 10, marginRight: 10, float: "left" },
+                  }}
+                >
+                  {tab_buttons[2]}
                 </div>
 
-                <div style={{...{float: 'right'}}}>
-                    {tab_buttons[3]}
-                </div>
+                <div style={{ ...{ float: "right" } }}>{tab_buttons[3]}</div>
               </div>
 
-              {/* TABLE */}
-              <div>
-                <div style={styles.row}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={{...styles.row, ...styles.odd_row}}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={styles.row}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={{...styles.row, ...styles.odd_row}}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={styles.row}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={{...styles.row, ...styles.odd_row}}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={styles.row}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={{...styles.row, ...styles.odd_row}}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={styles.row}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={{...styles.row, ...styles.odd_row}}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={styles.row}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-                <div style={{...styles.row, ...styles.odd_row}}>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                  <div style={{width: "100%"}}>ROW</div>
-                </div>
-              </div>
+            <div className="content">
+              {
+                Array.from({length: 30}).map((_, rowIndex) => (<>
+                  {rowIndex % 2 === 0 ? (
+                    <div style={{ ...styles.row }}>
+                      <div style={{ width: "100%" }}>ROW</div>
+                      <div style={{ width: "100%" }}>ROW</div>
+                      <div style={{ width: "100%" }}>ROW</div>
+                      <div style={{ width: "100%" }}>ROW</div>
+                      <div style={{ width: "100%" }}>ROW</div>
+                      <div style={{ width: "100%" }}>ROW</div>
+                    </div>
+                  ) : (
+                    <div style={{ ...styles.row, ...styles.odd_row }}>
+                    <div style={{ width: "100%" }}>ROW</div>
+                    <div style={{ width: "100%" }}>ROW</div>
+                    <div style={{ width: "100%" }}>ROW</div>
+                    <div style={{ width: "100%" }}>ROW</div>
+                    <div style={{ width: "100%" }}>ROW</div>
+                    <div style={{ width: "100%" }}>ROW</div>
+                  </div>
+                  )}
+                </>))
+              }
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 const styles = {
-    selected_button: {
-      borderBottomWidth: 2,
-      borderBottomColor: 'black',
-    },
-    row: {
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      minHeight: 35,
-      borderRadius: 25,
-      background: '#D9D9D9'
-    },
-    odd_row: {
-      borderRadius: 25,
-      background: 'transparent'
-    },
-    background: { 
-      backgroundImage: `url(${background})`,
-      backgroundSize: "cover",
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      width: "100vw",
-      height: "100vh",
-      color: "black"
-    },
-    modal: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100%'
-    },
-    container: {
-      backgroundColor: "white",
-      borderRadius: '18px',
-      margin: "auto",
-      textAlign: "center",
-      position: "absolute",
-      top: 40, bottom: 40, right: 40, left: 40,
-      overflow: 'hidden',
-      
-      display: 'flex',
-    },
-    section: {
-      marginTop: 5
-    },
-    button: {
-      borderRadius: 25,
-      width: 150
-    },
-    text_input: {
-      width: "100%"
-    },
-    header: {
-      fontWeight: 'bold',
-      fontSize: 36
-    },
-    sub_header: {
-  
-    },
-    sidebar: {
-        backgroundColor: "#2673DD",
-        width: 260,
-        color: "white"
-    },
-    sidebar_button: {
-        width: '100%',
-        color: 'white',
-    },
-    tab_button: {
-        width: '100%',
-        color: 'black',
-    }
-  }
+  table: {
+    maxHeight: '100%',
+    overflowY: 'auto',
+  },
+  selected_button: {
+    borderBottomWidth: 2,
+    borderBottomColor: "black",
+  },
+  row: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: 35,
+    borderRadius: 25,
+    background: "#D9D9D9",
+  },
+  odd_row: {
+    borderRadius: 25,
+    background: "transparent",
+  },
+  background: {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    width: "100vw",
+    height: "100vh",
+    color: "black",
+  },
+  modal: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  container: {
+    backgroundColor: "white",
+    borderRadius: "18px",
+    margin: "auto",
+    textAlign: "center",
+    position: "absolute",
+    top: 40,
+    bottom: 40,
+    right: 40,
+    left: 40,
+    overflow: "hidden",
 
+    display: "flex",
+  },
+  section: {
+    marginTop: 5,
+  },
+  button: {
+    borderRadius: 25,
+    width: 150,
+  },
+  text_input: {
+    width: "100%",
+  },
+  header: {
+    fontWeight: "bold",
+    fontSize: 36,
+  },
+  sub_header: {},
+  sidebar: {
+    backgroundColor: "#2673DD",
+    width: 260,
+    color: "white",
+  },
+  sidebar_button: {
+    width: "100%",
+    color: "white",
+  },
+  tab_button: {
+    width: "100%",
+    color: "black",
+  },
+};
 
-export default Dashboard
+export default Dashboard;
