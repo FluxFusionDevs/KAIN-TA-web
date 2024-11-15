647afd84dbbb199f3323babb9529a46c6011d41f
ucelery <atondavid.ad@gmail.com>
2024-11-15 20:45:48 +0800

    Clean Unused Imports and Variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import './App.css'
-import background from './assets/images/background.png'
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { fontSize, fontWeight } from '@mui/system';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Dashboard from './dashboard';
+import Dashboard from './pages/superadmin/superadminView';
 import Login from './login';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
 
+function App() {
   return (
     <Router>
       <Routes>
@@ -20,50 +14,6 @@ function App() {
       </Routes>
     </Router>
   );
-}
-
-const styles = {
-  background: { 
-    backgroundImage: `url(${background})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    width: "100vw",
-    height: "100vh",
-    color: "black"
-  },
-  modal: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%'
-  },
-  container: {
-    backgroundColor: "white",
-    borderRadius: '18px',
-    margin: "auto",
-    padding: 35,
-    display: 'inline-block',
-    width: 260,
-    textAlign: "center"
-  },
-  section: {
-    marginTop: 5
-  },
-  button: {
-    borderRadius: 25,
-    width: 150
-  },
-  text_input: {
-    width: "100%"
-  },
-  header: {
-    fontWeight: 'bold',
-    fontSize: 36
-  },
-  sub_header: {
-
-  },
 }
 
 export default App
