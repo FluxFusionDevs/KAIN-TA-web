65b4a419eb09f8e49dab903b91c8ef696c215d9b
ucelery <atondavid.ad@gmail.com>
2024-11-16 10:22:42 +0800

    Add Description Property; Include Add Button for Adding Food

--- src/pages/admin/menuPage.tsx
+++ src/pages/admin/menuPage.tsx
@@ -1,19 +1,58 @@
 import { useState } from "react";
-import { Button } from "@mui/material";
-import ThumbUp from "@mui/icons-material/ThumbUp";
+import { Button, TextField } from "@mui/material";
+import { ThumbDown, ThumbUp } from "@mui/icons-material";
+
+import Modal from "../../components/users_modal";
 
 import './menuPage.css'
-import { ThumbDown } from "@mui/icons-material";
+
+enum DashboardState {
+  Idle, IsAdding, IsEditting, IsDeleting, IsSaving
+}
 
 function DashboardPage() {
   const [selectedRow, setSelectedRow] = useState<number>(0);
+  const [state, setState] = useState<DashboardState>(DashboardState.Idle);
+    
+  const user_inputs = [
+    <div style={styles.section}>
+      <TextField style={styles.text_input} id="text_input" label="Name" variant="standard" />
+    </div>,
+    <div style={styles.section}>
+      <TextField style={styles.text_input} id="text_input" label="Description" variant="standard" />
+    </div>,
+    <div style={styles.section}>
+      <TextField style={styles.text_input} id="text_input" label="Tags" variant="standard" />
+    </div>,
+    <div style={styles.section}>
+      <TextField style={styles.text_input} id="text_input" label="Image" variant="standard" />
+    </div>,
+    <div style={styles.section}>
+      <TextField style={styles.text_input} id="text_input" label="Price" variant="standard" />
+    </div>,
+  ]
+
 
   return (
     <div className="verification-wrapper">
+      {state === DashboardState.IsAdding ? (
+        <Modal 
+          header="ADD FOOD" 
+          content={(user_inputs)}
+          onSubmit={() => setState(DashboardState.IsSaving)}
+          onCancel={() => setState(DashboardState.Idle)}
+          />
+      ) : []}
       <div className="table">
+        <Button 
+          style={{ ...styles.tab_button, background: "#2673DD", color: "white", width: 200, borderRadius: 25, marginBottom: 20 }}
+          onClick={() => setState(DashboardState.IsAdding)}>
+          add
+          </Button>
             <div className="content">
               <div className="header row row-header">
                 <div style={{ width: "100%" }}>Name</div>
+                <div style={{ width: "100%" }}>Description</div>
                 <div style={{ width: "100%" }}>Tags</div>
                 <div style={{ width: "100%" }}>Image</div>
                 <div style={{ width: "100%" }}>Price</div>
@@ -24,6 +63,7 @@ function DashboardPage() {
                   const class_name = rowIndex % 2 === 0 ? "row odd-row" : "row";
 
                   return (<div className={class_name} onClick={() => setSelectedRow(rowIndex)}>
+                    <div>ROW</div>
                     <div>ROW</div>
                     <div>ROW</div>
                     <div>ROW</div>
