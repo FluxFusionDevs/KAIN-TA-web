0b20fe51fd4902e1a48614cbbc79de49915e95fa
ucelery <atondavid.ad@gmail.com>
2024-11-24 19:00:30 +0800

    Implement Superadmin Dashboard

--- src/pages/superadmin/dashboardPage.tsx
+++ src/pages/superadmin/dashboardPage.tsx
@@ -1,109 +1,98 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import { Chart } from "react-google-charts";
 
 import './dashboardPage.css'
+import { getPayments } from "../../handlers/APIController";
 
 function DashboardPage() {
-  const [selectedRow, setSelectedRow] = useState<number>(0);
+  const [paymentData, setPaymentData] = useState<Array<[string, number]>>([]);
+  const [revenueData, setRevenueData] = useState<Array<[string, number]>>([]);
+
+  // Payment Revenue
+  useEffect(() => {
+    const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+    const fetch = async () => {
+      const data = await getPayments();
+      const dateCounts: Record<string, number> = {};
+      const result: Array<[string, number]> = [];
+
+    
+      data.forEach((item) => {
+        const isoString = item.createdAt;
+        const date = new Date(isoString);
+        const curr_date = `${months[date.getMonth() + 1]} ${date.getDate()}`;
+
+      
+        dateCounts[curr_date] = (dateCounts[curr_date] || 0) + 1;
+      });
+
+    
+      for (const [date, count] of Object.entries(dateCounts)) {
+        result.push([date, count]);
+      }
+
+      setPaymentData(result);
+    }
+
+    const fetchRevenue = async () => {
+      const data = await getPayments();
+      const dateCounts: Record<string, number> = {};
+      const result: Array<[string, number]> = [];
+      
+      data.forEach((item) => {
+        const isoString = item.createdAt;
+        const date = new Date(isoString);
+        const curr_date = `${months[date.getMonth() + 1]} ${date.getDate()}`;
+
+        dateCounts[curr_date] = (dateCounts[curr_date] || 0) + item.amount;
+      });
+
+      
+      for (const [date, count] of Object.entries(dateCounts)) {
+        result.push([date, count]); 
+      }
+
+      setRevenueData(result);
+      }
+
+    fetch();
+    fetchRevenue();
+  }, [])
 
   return (
-    <div className="wrapper">
+    <div className="super-dashboard-wrapper">
       {/* MORE CHARTS HERE https://www.react-google-charts.com/examples */}
-      <Chart
-        chartType="LineChart"
-        data={[
-          ["Day", "Subscribers"],
-          [1, 3],
-          [2, 7],
-          [3, 1],
-          [4, 11],
-          [6, 3],
-          [7, 8],
-          [8, 3],
-          [9, 11],
-          [10, 4],
-          [11, 3],
-          [12, 16],
-          [13, 11],
-          [14, 25],
-          [15, 20],
-          [16, 19],
-          [17, 23],
-        ]}
-        width="100%"
-        height="400px"
-        legendToggle />
+      <div className="row">
+        <div className="section">
+          <div className="header">
+            Amount of Subscribers
+          </div>
+          <Chart
+            style={{padding: 0, margin: 0}}
+            chartType="LineChart"
+            data={[
+              ["Day", "Subscribers"],
+              ...paymentData
+            ]}
+            legendToggle />
+        </div>
 
-      <Chart
-        chartType="Bar"
-        data={[
-          ["Day", "Subscribers"],
-          [1, 3],
-          [2, 7],
-          [3, 1],
-          [4, 11],
-          [6, 3],
-          [7, 8],
-          [8, 3],
-          [9, 11],
-          [10, 4],
-          [11, 3],
-          [12, 16],
-          [13, 11],
-          [14, 25],
-          [15, 20],
-          [16, 19],
-          [17, 23],
-        ]}
-        width="100%"
-        height="400px"
-        legendToggle />
+        <div className="section">
+        <div className="header">
+            Revenue
+          </div>
+        <Chart
+          chartType="Bar"
+          data={[
+            ["Day", "Subscribers"],
+            ...revenueData
+          ]}
+          legendToggle />
+        </div>
+      </div>
     </div>
   );
 }
-
-const styles = {
-  table: {
-    maxHeight: '100%',
-    overflowY: 'auto',
-  },
-  selected_button: {
-    borderBottomWidth: 2,
-    borderBottomColor: "black",
-  },
-  section: {
-    marginTop: 5,
-  },
-  button: {
-    borderRadius: 25,
-    width: 150,
-  },
-  text_input: {
-    width: "100%",
-  },
-  header: {
-    fontWeight: "bold",
-    fontSize: 36,
-  },
-  sub_header: {},
-  sidebar: {
-    backgroundColor: "#2673DD",
-    width: 260,
-    color: "white",
-  },
-  selected_sidebar_button: {
-    backgroundColor: 'rgba(0, 0, 0, 0.2)'
-  },
-  sidebar_button: {
-    width: "100%",
-    color: "white",
-    borderRadius: 20
-  },
-  tab_button: {
-    width: "100%",
-    color: "black",
-  },
-};
 
 export default DashboardPage;
